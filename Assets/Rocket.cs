23530fa29a0bdd3442a3cee1834dfb801673a397
Andrew Styffe <andrewstyffe3@gmail.com>
2020-01-24 13:55:24 -0800

    Added thrusting functionality to the rocket ship, as well as a sound clip

---
+++ Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		processInput();
+	}
+
+	void processInput() {
+
+		// Thrust while rotating
+		if(Input.GetKey(KeyCode.Space)) {
+			rigidBody.AddRelativeForce(Vector3.up);
+			// So it doesn't layer
+			if(!audioSource.isPlaying) {
+				audioSource.Play();
+			}
+		} else {
+			audioSource.Stop();
+		}
+
+		if(Input.GetKey(KeyCode.A)) {
+			transform.Rotate(Vector3.forward);
+		} else if(Input.GetKey(KeyCode.D)) {
+			transform.Rotate(-Vector3.forward);
+		} 
+	}
+}
